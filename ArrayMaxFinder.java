273ec811769f8c34e5c0c2cf4b28e4bdd02855e6
Ankur Yadav <ayts7667@gmail.com>
2026-02-18 11:18:25 +0530

    find max with array

---
+++ ArrayMaxFinder.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ArrayMaxFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int max = findMax(arr);
+        System.out.println("The maximum element in the array is: " + max);
+    }
+
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+}
